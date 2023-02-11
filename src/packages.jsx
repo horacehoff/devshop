529f7d795e244909bbb8c349e40625ec7037e748
just_a_mango <horace.hoff@gmail.com>
2023-02-11 22:55:02 +0100

    /

--- src/packages.jsx
+++ src/packages.jsx
@@ -6,8 +6,8 @@ export default function Packages() {
     return (
         <>
             <Navbar/>
-            <h1 className="buy-code-sell">PACKAGES</h1>
-            <h2>// CURRENTLY TRENDING</h2>
+            <h1 className="packages-title">PACKAGES</h1>
+            <h2 className="category-title">// CURRENTLY TRENDING</h2>
             <ul className="packages-card-list">
                 <li><PackageCard dwnl="55566564" author="hombrewTeam"
                                  desc="Homebrew offers a wide variety of packages to download. Its available platforms are macOS and Linux, and Windows support is coming soon"/>
