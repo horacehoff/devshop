13a7cc0cf7079f73d81b4f4086346bc867e105a3
just_a_mango <horace.hoff@gmail.com>
2023-02-11 23:27:00 +0100

    /

--- src/packages.jsx
+++ src/packages.jsx
@@ -21,6 +21,18 @@ export default function Packages() {
                 <li><PackageCard dwnl="55566564" author="hombrewTeam"
                                  desc="Homebrew offers a wide variety of packages to download. Its available platforms are macOS and Linux, and Windows support is coming soon"/>
                 </li>
+                <li><PackageCard dwnl="55566564" author="hombrewTeam"
+                                 desc="Homebrew offers a wide variety of packages to download. Its available platforms are macOS and Linux, and Windows support is coming soon"/>
+                </li>
+                <li><PackageCard dwnl="55566564" author="hombrewTeam"
+                                 desc="Homebrew offers a wide variety of packages to download. Its available platforms are macOS and Linux, and Windows support is coming soon"/>
+                </li>
+                <li><PackageCard dwnl="55566564" author="hombrewTeam"
+                                 desc="Homebrew offers a wide variety of packages to download. Its available platforms are macOS and Linux, and Windows support is coming soon"/>
+                </li>
+                <li><PackageCard dwnl="55566564" author="hombrewTeam"
+                                 desc="Homebrew offers a wide variety of packages to download. Its available platforms are macOS and Linux, and Windows support is coming soon"/>
+                </li>
             </ul>
         </>
     )
