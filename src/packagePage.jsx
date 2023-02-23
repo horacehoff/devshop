a8f0c49dfce2b74c6a40cf176db0067d7bdf2f17
just_a_mango <horace.hoff@gmail.com>
2023-02-23 22:12:15 +0100

    /

--- src/packagePage.jsx
+++ src/packagePage.jsx
@@ -8,6 +8,7 @@ export default function PackagePage() {
             <div className="banner"></div>
             <h2 className="package-title">Homebrew</h2>
             <h3 className="package-author">// BY <span style={{color: "#F0EBBA"}}>HomebrewTEAM</span></h3>
+            <button className="package-download-btn">{"DOWNLOAD -> 0$"}</button>
             <p className="package-description-label">// 01 - DESCRIPTION</p>
             <p className="package-description">Homebrew offers a wide variety of packages to download. Its available
                 platforms are macOS and Linux, and Windows support is coming soonHomebrew offers a wide variety of
@@ -21,14 +22,18 @@ export default function PackagePage() {
                 macOS and Linux, and Windows support is coming soonnonsoon</p>
             <p className="package-screenshots-label"></p>
             <div className="package-screenshots">
-                <img src={"https://brew.sh/assets/img/homebrew-social-card.png"} className="package-img-one"/>
-                <img src={"https://brew.sh/assets/img/homebrew-social-card.png"} className="package-img-two"/>
-                <img src={"https://brew.sh/assets/img/homebrew-social-card.png"} className="package-img-three"/>
-                <img src={"https://brew.sh/assets/img/homebrew-social-card.png"} className="package-img-four"/>
+                <img src={"https://brew.sh/assets/img/homebrew-social-card.png"} className="package-img"/>
+                <img src={"https://brew.sh/assets/img/homebrew-social-card.png"} className="package-img"
+                     style={{marginLeft: "25px"}}/>
+                <img src={"https://brew.sh/assets/img/homebrew-social-card.png"} className="package-img"
+                     style={{marginLeft: "25px"}}/>
+                <img src={"https://brew.sh/assets/img/homebrew-social-card.png"} className="package-img"
+                     style={{marginLeft: "25px", marginRight: "15px"}}/>
             </div>
             <p className="package-characteristics-label"></p>
             <div className="package-characteristics">
-                <p>TOTAL DOWNLOADS: 1.45K<br/>AVERAGE HAPPINESS: 100%<br/>TOTAL SIZE: 5.8MB</p>
+                <p>TOTAL DOWNLOADS: 1.45K<br/>AVERAGE HAPPINESS: 100%<br/>TOTAL SIZE: 5.8MB<br/>CURRENT VERSION: 0.X.X
+                </p>
             </div>
         </>
     )
