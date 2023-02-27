6f27bd090692f1371257542bd2c5ca5219c403c3
Just_a_Mango <horace.hoff@gmail.com>
2023-02-27 23:58:22 +0100

    /

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,17 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  // add terser to build.target
+    build: {
+        target: 'esnext',
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                keep_infinity: true,
+                pure_getters: true,
+                passes: 10,
+            }
+        }
+    }
 })
