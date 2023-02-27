4d4aa94821b0cc9f7b105ab403a6ae04fa284fe0
Just_a_Mango <horace.hoff@gmail.com>
2023-02-28 00:30:12 +0100

    /

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  // add terser to build.target
     build: {
         target: 'esnext',
         minify: 'terser',
@@ -24,12 +23,30 @@ export default defineConfig({
                 unsafe_regexp: true,
                 unsafe_undefined: true,
                 warnings: false,
-
+              // compress more
+                booleans_as_integers: true,
+                collapse_vars: true,
+                comparisons: true,
+                computed_props: true,
+                hoist_funs: true,
+                hoist_props: true,
+                hoist_vars: true,
+                if_return: true,
+                inline: true,
+                join_vars: true,
+                keep_fargs: false,
+                keep_fnames: false,
+                loops: true,
+                negate_iife: false,
+                properties: true,
+                reduce_funcs: true,
+                reduce_vars: true
             },
             mangle: {
                 properties: {
                     regex: /^_/,
-                }
+
+                },
             },
             output: {
                 comments: false,
