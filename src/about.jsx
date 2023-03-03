1faa20b7cfadf21d70d83ec61e3faac61ca2673e
just_a_mango <horace.hoff@gmail.com>
2023-03-03 12:20:49 +0100

    /

--- src/about.jsx
+++ src/about.jsx
@@ -6,8 +6,10 @@ export default function About() {
         <>
         <Navbar/>
         <h2 className="about-title">ABOUT ‘DEVSHOP’</h2>
-        <p className="about-subtitle">BY @JUST_A_MANGO</p>
-        <p className="about-subtitle" style={{marginTop: "15px"}}>DEVELOPMENT STARTED IN FEBRUARY 2023</p>
+            <p className="about-subtitle">BY <span className="about-linktree"
+                                                   onClick={() => window.open("https://linktr.ee/just_a_mango", '_blank').focus()}>@JUST_A_MANGO</span>
+            </p>
+            <p className="about-subtitle" style={{marginTop: "15px"}}>DEVELOPMENT STARTED IN FEBRUARY 2023</p>
         </>
     )
 }
