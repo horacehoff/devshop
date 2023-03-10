056463c9ceafc258458b7b0bbde701bc4eb8c915
just_a_mango <horace.hoff@gmail.com>
2023-03-10 21:28:22 +0100

    /

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,16 @@ export default function Home() {
             <br/>
             <h1 className="buy-code-sell">CODE.<br/>BUY.<br/>SELL.</h1>
             <p className="one-catchphrase">// 01 - CATCHPHRASE</p>
-            <p className="one-desc">{"DEVSHOP allows anyone to buy, use, or sell code. Wether it be full programs (called <<packages>>) or code snippets (called <<code blocks>>), DEVSHOP has it all."}</p>
+            <p className="one-desc">{"DEVSHOP allows anyone to buy, use, or sell code. Wether it be full programs {called <<packages>>} or code snippets {called <<code blocks>>}, DEVSHOP has it all."}</p>
+            <p className="one-catchphrase">// 02 - WHY USE DEVSHOP ?</p>
+            <p className="one-desc">
+                <ul>
+                    <li>DEVSHOP is free to use {"{except if you want to remove ads}"}</li>
+                    <li>DEVSHOP charges a commission of 10% only per sale</li>
+                    <li>DEVSHOP is developer-friendly</li>
+                    <li>DEVSHOP will never sell your personal information</li>
+                </ul>
+            </p>
         </>
     )
 }
