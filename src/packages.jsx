c7c9bdc04013001001197a2e3376773487303a9e
just_a_mango <horace.hoff@gmail.com>
2023-04-22 13:23:27 +0200

    /

--- src/packages.jsx
+++ src/packages.jsx
@@ -1,9 +1,8 @@
 import "./packages.css"
 import Navbar from "./Navbar.jsx";
-import PackageCard from "./packageCard.jsx";
 import {db} from "./firebase.js";
 import {useEffect, useState} from "react";
-import {collection, doc, getDoc, getDocs, limit, orderBy, query, setLogLevel} from "firebase/firestore";
+import {collection, getDocs, limit, orderBy, query, setLogLevel} from "firebase/firestore";
 import {useNavigate} from "react-router-dom";
 
 
@@ -34,43 +33,41 @@ export default function Packages() {
 
     const [packages, setPackages] = useState([]);
 
-    const fetchPackages = () => {
-        console.log("fetching packages...");
-        const collectionRef = collection(db, 'packages');
-        console.log("ordering packages...");
-        const q = query(collectionRef, orderBy('downloads', 'desc'), orderBy('created', 'desc'), limit(9));
-        console.log("getting packages.../async");
-        return getDocs(q)
-            .then(querySnapshot => {
-                console.log("packages fetched -> setPackages");
-                const packageData = querySnapshot.docs.map(doc => doc.data());
-                setPackages(packageData);
-            })
-            .catch(error => {
-                console.log('Error getting documents: ', error);
-            });
-    };
+
     useEffect(() => {
-        fetchPackages()
-            .then(() => {
-                console.log("fetchPackages completed successfully");
-            })
-            .catch(error => {
-                console.log("Error fetching packages: ", error);
-            });
+        const fetchPackages = () => {
+            console.log("fetching packages...");
+            const collectionRef = collection(db, 'packages');
+            console.log("ordering packages...");
+            const q = query(collectionRef, orderBy('downloads', 'desc'), orderBy('created', 'desc'), limit(9));
+            console.log("getting packages.../async");
+            getDocs(q)
+                .then(querySnapshot => {
+                    console.log("packages fetched -> setPackages");
+                    const packageData = querySnapshot.docs.map(doc => doc.data());
+                    setPackages(packageData);
+                })
+                .catch(error => {
+                    console.log('Error getting documents: ', error);
+                });
+        };
+
+
+        fetchPackages();
+        console.log("fetch packages end call ")
     }, []);
 
-    const getUsername = async (userId) => {
-        const userDocRef = doc(collection(db, 'users'), userId);
-        const userDocSnapshot = await getDoc(userDocRef);
-
-        if (userDocSnapshot.exists()) {
-            const userData = userDocSnapshot.data();
-            return userData.username;
-        } else {
-            throw new Error(`User with ID ${userId} not found`);
-        }
-    };
+    // const getUsername = async (userId) => {
+    //     const userDocRef = doc(collection(db, 'users'), userId);
+    //     const userDocSnapshot = await getDoc(userDocRef);
+    //
+    //     if (userDocSnapshot.exists()) {
+    //         const userData = userDocSnapshot.data();
+    //         return userData.username;
+    //     } else {
+    //         throw new Error(`User with ID ${userId} not found`);
+    //     }
+    // };
 
     return (
         <>
@@ -78,14 +75,14 @@ export default function Packages() {
             <h1 className="packages-title">PACKAGES</h1>
             <h2 className="category-title">// CURRENTLY TRENDING</h2>
             <ul className="packages-card-list" id="packages-card-list">
-                {packages.map((pkg, index) => (
-                    <li key={index} className="packages-card-list-child" onClick={() => {
-                        navigate("/" + pkg.name)
-                    }}>
-                        <PackageCard dwnl={pkg.downloads} author={pkg.owner_username} name={pkg.name}
-                                     desc={pkg.description} banner={pkg.banner}/>
-                    </li>
-                ))}
+                {/*{packages.map((pkg, index) => (*/}
+                {/*    <li key={index} className="packages-card-list-child" onClick={() => {*/}
+                {/*        navigate("/" + pkg.name)*/}
+                {/*    }}>*/}
+                {/*        <PackageCard dwnl={pkg.downloads} author={pkg.owner_username} name={pkg.name}*/}
+                {/*                     desc={pkg.description} banner={pkg.banner}/>*/}
+                {/*    </li>*/}
+                {/*))}*/}
             </ul>
             <h2 className="category-title">// RECENTLY CREATED</h2>
             <ul className="packages-card-list" id="packages-card-list">
