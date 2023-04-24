f75b42858dbf7e1717a20249b73bc97df9c95c28
Just_a_Mango <horace.hoff@gmail.com>
2023-04-24 15:26:07 +0200

    /

--- src/firebase.js
+++ src/firebase.js
@@ -24,7 +24,7 @@ export const auth = initializeAuth(app, {
 
 console.log("init firestore")
 export const db = initializeFirestore(app, {
-    experimentalForceLongPolling: false
+    experimentalForceLongPolling: true
 });
 
 
