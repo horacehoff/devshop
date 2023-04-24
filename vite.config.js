270395e94ba11b956525ca851c31e1da65858a1f
Just_a_Mango <horace.hoff@gmail.com>
2023-04-24 15:46:03 +0200

    /

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',
     minify: 'terser',
-    terserOptions: {
-      compress: {
-        keep_infinity: true,
-        pure_getters: true,
-        passes: 100,
-        drop_console: false,
-        drop_debugger: false,
-        warnings: true,
-      },
-      mangle: {
-        properties: {
-          regex: /^_/,
-        },
-      },
-      output: {
-        comments: false,
-        beautify: false,
-      },
-    }
   }
 })
