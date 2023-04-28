886c5f1cccb0c78b6f4c339c2c6357057e4bfaa0
just_a_mango <horace.hoff@gmail.com>
2023-04-28 21:57:07 +0200

    /

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,40 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import terser from "@rollup/plugin-terser";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: 'terser',
-    sourcemap: false,
-    terserOptions: {
+  plugins: [
+    react(),
+    terser({
       compress: {
         passes: 1000,
         drop_console: true,
       },
-
-    },
+      format: {
+        comments: false,
+      },
+    }),
+  ],
+  build: {
+    sourcemap: false,
     rollupOptions: {
       output: {
         manualChunks: {
-          "main": ['react'],
-          'maindom': ['react-dom'],
-          'mainrouterdom': ['react-router-dom'],
-          'app': ['firebase/app'],
-          'auth': ['firebase/auth'],
-          'store': ['firebase/firestore'],
-          'storage': ['firebase/storage'],
-          'codehighlight': ['react-syntax-highlighter'],
-          'shortnum': ['short-number'],
-          'popup': ['reactjs-popup'],
+          main: ['react'],
+          maindom: ['react-dom'],
+          mainrouterdom: ['react-router-dom'],
+          app: ['firebase/app'],
+          auth: ['firebase/auth'],
+          store: ['firebase/firestore'],
+          storage: ['firebase/storage'],
+          codehighlight: ['react-syntax-highlighter'],
+          shortnum: ['short-number'],
+          popup: ['reactjs-popup'],
         },
         compact: true,
         minifyInternalExports: true,
-      }
-    }
-  }
-})
+      },
+      plugins: [terser()],
+    },
+  },
+});
