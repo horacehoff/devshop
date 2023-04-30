6ef8393b71daa5e68c001ab42fbffb1495ef18f6
just_a_mango <horace.hoff@gmail.com>
2023-04-30 13:23:11 +0200

    /

--- src/accountPage.jsx
+++ src/accountPage.jsx
@@ -2,18 +2,39 @@ import "./accountPage.css"
 import Navbar from "./Navbar.jsx";
 import {SiGithub} from "react-icons/si";
 import fancy_name_to_id from "./utility.js";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import {collection, getDocs, query, where} from "firebase/firestore";
+import {db} from "./firebase.js";
+import PackageCard from "./packageCard.jsx";
 
 export default function AccountPage(props) {
     const usr = props.user;
+    const [usrPackages, setUsrPackages] = useState([])
 
     useEffect(() => {
         if (usr.github === "") {
             document.getElementById("user_github").style.display = "none";
             document.getElementById("user_id_middle_dot").style.display = "none";
         }
-    }, []);
 
+        const getUsrPackages = () => {
+            const citiesRef = collection(db, "packages");
+            const q = query(citiesRef, where("owner_id", "==", usr.uid));
+            const querySnapshot = getDocs(q).then((snap) => {
+                snap.forEach((doc) => {
+                    console.log(doc.data())
+                    let packages = usrPackages
+                    packages.push(doc.data())
+                    setUsrPackages(packages)
+                });
+            })
+        }
+        getUsrPackages();
+        console.log(usrPackages)
+
+
+        //
+    }, []);
 
     return (
         <>
@@ -25,6 +46,16 @@ export default function AccountPage(props) {
                 className="user_github" id="user_github"><SiGithub className="user_github_icon"/><span
                 className="user_github_id">{usr.github}</span></span></p>
             <p className="user_bio">{usr.bio}</p>
+            <ul className="packages-card-list" id="packages-card-list" style={{marginTop: "450px"}}>
+                {usrPackages.map((pkg, index) => (
+                    <li key={index} className="packages-card-list-child" onClick={() => {
+                        navigate("/packages/" + fancy_name_to_id(pkg.name))
+                    }}>
+                        <PackageCard dwnl={pkg.downloads} author={pkg.owner_username} name={pkg.name}
+                                     desc={pkg.description} banner={pkg.banner}/>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 
