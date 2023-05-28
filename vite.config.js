771746e6b5bcea618916dc5615fff522cb43d489
just_a_mango <horace.hoff@gmail.com>
2023-05-28 11:57:41 +0200

    /

--- vite.config.js
+++ vite.config.js
@@ -54,7 +54,7 @@ export default defineConfig({
         comments: false,
       },
       compress: {
-        passes: 1000, // Adjust the number of compression passes as needed
+        passes: 10, // Adjust the number of compression passes as needed
         drop_console: true,
         toplevel: true,
       },
@@ -65,20 +65,30 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks(id, {getModuleInfo}) {
-          const {size} = getModuleInfo(id);
-          if (size > 50000) {
-            // Only split packages larger than 50kB into separate chunks
-            if (id.includes('node_modules')) {
-              const packageName = id.match(/\/node_modules\/([^/]+)/)[1];
-              return packageName.replace('@', '');
-            }
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Split each dependency into separate chunks
+            const packageName = id.match(/\/node_modules\/([^/]+)/)[1];
+            return packageName.replace('@', '');
           }
         },
         compact: true,
         minifyInternalExports: true,
+        entryFileNames: 'assets/[name]-[hash].js', // Customize output file names
+        chunkFileNames: 'assets/[name]-[hash].js', // Customize chunk file names
       },
-      plugins: [terser()],
+      plugins: [
+        terser({
+          format: {
+            comments: false,
+          },
+          compress: {
+            passes: 10, // Adjust the number of compression passes as needed
+            drop_console: true,
+            toplevel: true,
+          },
+        }),
+      ],
     },
   },
 });
