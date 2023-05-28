4f1f589446c88838fbf0daba3b6c4bc2c15f29f4
just_a_mango <horace.hoff@gmail.com>
2023-05-28 11:50:06 +0200

    /

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,62 @@
+// import {defineConfig} from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+// import terser from "@rollup/plugin-terser";
+//
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     terser({
+//       compress: {
+//         passes: 1000,
+//         drop_console: true,
+//         toplevel: true
+//       },
+//       format: {
+//         comments: false,
+//       },
+//     }),
+//   ],
+//   build: {
+//     minify: 'terser',
+//     sourcemap: false,
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           main: ['react'],
+//           maindom: ['react-dom'],
+//           mainrouterdom: ['react-router-dom'],
+//           app: ['firebase/app'],
+//           auth: ['firebase/auth'],
+//           store: ['firebase/firestore'],
+//           storage: ['firebase/storage'],
+//           shortnum: ['short-number'],
+//           popup: ['reactjs-popup'],
+//           icons: ['react-icons'],
+//           markdown: ['@uiw/react-md-editor'],
+//         },
+//         compact: true,
+//         minifyInternalExports: true,
+//       },
+//       plugins: [terser()],
+//     },
+//   },
+// });
+
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import terser from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser"
 
 export default defineConfig({
   plugins: [
     react(),
     terser({
-      compress: {
-        passes: 1000,
-        drop_console: true,
-      },
       format: {
         comments: false,
+      },
+      compress: {
+        passes: 10, // Adjust the number of compression passes as needed
+        drop_console: true,
+        toplevel: true,
       },
     }),
   ],
@@ -20,18 +65,12 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: {
-          main: ['react'],
-          maindom: ['react-dom'],
-          mainrouterdom: ['react-router-dom'],
-          app: ['firebase/app'],
-          auth: ['firebase/auth'],
-          store: ['firebase/firestore'],
-          storage: ['firebase/storage'],
-          shortnum: ['short-number'],
-          popup: ['reactjs-popup'],
-          icons: ['react-icons'],
-          markdown: ['@uiw/react-md-editor'],
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Split each dependency into separate chunks
+            const packageName = id.match(/\/node_modules\/([^/]+)/)[1];
+            return packageName.replace('@', '');
+          }
         },
         compact: true,
         minifyInternalExports: true,
