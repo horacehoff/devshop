fc746993c70695bed21bf863a8a6250ccfe4c700
just_a_mango <horace.hoff@gmail.com>
2023-05-29 11:27:51 +0200

    /

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,65 @@
 import React, {lazy, Suspense, useEffect, useState} from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "./firebase.js"
+import fancy_name_to_id from "./utility.js";
+
+
 import './index.css'
 import './lazy.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+
 import Home from "./Home.jsx";
 import Navbar from "./Navbar.jsx";
 import Packages from "./packages.jsx";
 import About from "./about.jsx";
 import CreatePackage from "./createPackage.jsx";
-import {collection, getDocs, query, setLogLevel} from "firebase/firestore";
 import Pricing from "./pricing.jsx";
-import "./firebase.js"
-import {db} from "./firebase.js";
 import PackagePage from "./packagePage.jsx";
 import SignUp from "./signup.jsx";
 import SignIn from "./signin.jsx";
 import ResetPassword from "./resetPassword.jsx";
 import AccountSettings from "./accountSettings.jsx";
-import fancy_name_to_id from "./utility.js";
 import AccountPage from "./accountPage.jsx";
 
-// lazy load
+
 const CodeBlocks = lazy(() => import('./codeBlocks.jsx'))
 
+
 async function getPackages(collectionRef) {
-    console.log("getting packages...")
+    console.log('getting packages...');
     try {
+        const {getDocs, query} = await import('firebase/firestore');
         const q = query(collectionRef);
         const querySnapshot = await getDocs(q);
-        console.log("packages fetched")
-        return querySnapshot.docs.map(doc => doc.data());
+        console.log('packages fetched');
+        return querySnapshot.docs.map((doc) => doc.data());
     } catch (error) {
         console.log('Error getting documents: ', error);
+        return [];
+    }
+}
+
+async function fetchPackagesData() {
+    try {
+        const {db} = await import('./firebase.js');
+        const {collection} = await import('firebase/firestore');
+        const collectionRef = collection(db, 'packages');
+        return await getPackages(collectionRef);
+    } catch (error) {
+        console.log('Error getting packages: ', error);
+        return [];
+    }
+}
+
+async function fetchUsersData() {
+    try {
+        const {db} = await import('./firebase.js');
+        const {collection} = await import('firebase/firestore');
+        const collectionRef = collection(db, 'users');
+        return await getPackages(collectionRef);
+    } catch (error) {
+        console.log('Error getting users: ', error);
         return [];
     }
 }
@@ -39,33 +67,20 @@ async function getPackages(collectionRef) {
 function App() {
     const [packages, setPackages] = useState([]);
     const [users, setUsers] = useState([]);
-    setLogLevel("debug");
+
 
     useEffect(() => {
-        console.log("fetching packages(function call)...")
-        const fetchPackages = async () => {
-            try {
-                const collectionRef = collection(db, 'packages');
-                const data = await getPackages(collectionRef);
-                setPackages(data);
-                console.log("packages fetched(function call)+setPackages done")
-            } catch (error) {
-                console.log('Error getting packages: ', error);
-            }
-        };
-        const fetchUsers = async () => {
-            try {
-                const collectionRef = collection(db, 'users');
-                const data = await getPackages(collectionRef);
-                setUsers(data);
-                console.log("users fetched(function call)+setUsers done")
-            } catch (error) {
-                console.log('Error getting users: ', error);
-            }
-        };
+        console.log('fetching packages (function call)...');
 
-        fetchPackages().then(r => console.log('Packages fetched'));
-        fetchUsers().then(r => console.log('Users fetched'));
+        fetchPackagesData().then((data) => {
+            setPackages(data);
+            console.log('packages fetched (function call) + setPackages done');
+        });
+
+        fetchUsersData().then((data) => {
+            setUsers(data);
+            console.log('users fetched (function call) + setUsers done');
+        });
     }, []);
 
     return (
