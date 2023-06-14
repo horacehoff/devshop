16e88792666480e00a751719e76b5f4c0c7e8d82
just_a_mango <horace.hoff@gmail.com>
2023-06-14 15:32:59 +0200

    /

--- src/utility.js
+++ src/utility.js
@@ -1,7 +1,6 @@
 export default function fancy_name_to_id(name) {
     let underscored = name.replace(/ /g, '_');
     let alphanumeric = underscored.replace(/\W/g, '');
-    console.log("FANCIED TO: " + alphanumeric.toLowerCase())
     return alphanumeric.toLowerCase();
 }
 
