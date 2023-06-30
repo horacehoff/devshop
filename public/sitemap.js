503810bcefbec3d725facf0a589315497189cc3b
just_a_mango <horace.hoff@gmail.com>
2023-06-30 19:13:55 +0200

    /

--- public/sitemap.js
+++ public/sitemap.js
@@ -1,5 +1,5 @@
 import GenerateSitemap from "react-router-sitemap-maker";
-import {MyRoutes} from "../src/routes";
+import {MyRoutes} from "./routes.jsx";
 
 const sitemapData = await GenerateSitemap(MyRoutes(), {
     baseUrl: "https://dev-shop.vercel.app",
