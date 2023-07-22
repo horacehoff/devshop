c193fe38b6acef125dd794102e60fa652f9c879c
just_a_mango <horace.hoff@gmail.com>
2023-07-22 12:08:55 +0200

    /

--- src/about.jsx
+++ src/about.jsx
@@ -3,7 +3,7 @@ import "./about.css";
 export default function About() {
     return (
         <>
-            <h2 className="about-title">ABOUT ‘DEVSHOP’</h2>
+            <h2 className="about-ttl">ABOUT ‘DEVSHOP’</h2>
             <p className="about-subtitle">BY <span className="about-linktree"
                                                    onClick={() => window.open("https://linktr.ee/just_a_mango", '_blank').focus()}>@JUST_A_MANGO</span>
             </p>
