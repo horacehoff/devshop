40f57ca114724a33adb825087c47093c47ac89ff
just_a_mango <horace.hoff@gmail.com>
2023-08-02 09:28:00 +0300

    /

--- vite.config.js
+++ vite.config.js
@@ -1,77 +1,77 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import viteCompression from 'vite-plugin-compression';
-import terser from "@rollup/plugin-terser";
+// import viteCompression from 'vite-plugin-compression';
+// import terser from "@rollup/plugin-terser";
 
 export default defineConfig({
-  optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      'react-router-dom',
-      'firebase/app',
-      'firebase/auth',
-      'firebase/firestore',
-      'firebase/storage',
-      'short-number',
-      'react-icons',
-      '@uiw/react-md-editor',
-      'reactjs-popup'
-    ],
-  },
+  // optimizeDeps: {
+  //   include: [
+  //     'react',
+  //     'react-dom',
+  //     'react-router-dom',
+  //     'firebase/app',
+  //     'firebase/auth',
+  //     'firebase/firestore',
+  //     'firebase/storage',
+  //     'short-number',
+  //     'react-icons',
+  //     '@uiw/react-md-editor',
+  //     'reactjs-popup'
+  //   ],
+  // },
   plugins: [
     react(),
-    viteCompression({
-      verbose: true,
-      algorithm: "brotliCompress",
-    }),
-    terser({
-      compress: {
-        passes: 1000,
-        drop_console: true,
-        drop_debugger: true
-      },
-      format: {
-        comments: false,
-        source_map: false,
-      },
-    }),
+    // viteCompression({
+    //   verbose: true,
+    //   algorithm: "brotliCompress",
+    // }),
+    // terser({
+    //   compress: {
+    //     passes: 1000,
+    //     drop_console: true,
+    //     drop_debugger: true
+    //   },
+    //   format: {
+    //     comments: false,
+    //     source_map: false,
+    //   },
+    // }),
   ],
-  build: {
-    cssMinify: 'terser',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        passes: 1000
-      },
-      format: {
-        comments: false,
-        source_map: false,
-      },
-    },
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          main: ['react'],
-          maindom: ['react-dom'],
-          mainrouterdom: ['react-router-dom'],
-          app: ['firebase/app'],
-          auth: ['firebase/auth'],
-          store: ['firebase/firestore'],
-          storage: ['firebase/storage'],
-          shortnum: ['short-number'],
-          icons: ['react-icons'],
-          markdown: ['@uiw/react-md-editor'],
-          popups: ['reactjs-popup']
-        },
-        compact: true,
-        minifyInternalExports: true,
-        sourcemap: false,
-      },
-    },
-    plugins: [terser()],
-  },
+  // build: {
+  //   cssMinify: 'terser',
+  //   minify: 'terser',
+  //   terserOptions: {
+  //     compress: {
+  //       drop_console: true,
+  //       drop_debugger: true,
+  //       passes: 1000
+  //     },
+  //     format: {
+  //       comments: false,
+  //       source_map: false,
+  //     },
+  //   },
+  //   sourcemap: false,
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         main: ['react'],
+  //         maindom: ['react-dom'],
+  //         mainrouterdom: ['react-router-dom'],
+  //         app: ['firebase/app'],
+  //         auth: ['firebase/auth'],
+  //         store: ['firebase/firestore'],
+  //         storage: ['firebase/storage'],
+  //         shortnum: ['short-number'],
+  //         icons: ['react-icons'],
+  //         markdown: ['@uiw/react-md-editor'],
+  //         popups: ['reactjs-popup']
+  //       },
+  //       compact: true,
+  //       minifyInternalExports: true,
+  //       sourcemap: false,
+  //     },
+  //   },
+  //   plugins: [terser()],
+  // },
 });
