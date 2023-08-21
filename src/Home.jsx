d484e28d382ec8a83633891eb3d7af6ee38876da
just_a_mango <horace.hoff@gmail.com>
2023-08-21 13:14:47 +0200

    /

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,20 @@
 import "./Home.css"
 import Footer from "./Footer.jsx";
+import {Helmet} from "react-helmet";
 
 
 export default function Home() {
     return (
         <>
             {/*<br/>*/}
+            <Helmet>
+                <meta
+                    content="Your one-stop-shop for buying, using, or selling code, wether that be full programs or code snippets, for free or not."
+                    name="description"/>
+                <meta
+                    content="shop, marketplace, developer, program, programs, code, snippets, sell, buy, coding, sell code, buy code, indie, indie dev"
+                    name="keywords"/>
+            </Helmet>
             <h1 className="buy-code-sell">CODE.<br/>BUY.<br/>SELL.</h1>
             <p className="one-catchphrase">// 01 - CATCHPHRASE</p>
             <p className="one-desc"
